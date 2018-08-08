d10342b415bd6bd1a2a886d3c9ba3f45bc10586a
Jonathan Quintero <quinterojosuej@gmail.com>
2018-08-08 00:21:32 +0000

    Api set up

--- project/main.py
+++ project/main.py
@@ -2,16 +2,34 @@ import webapp2
 from random import shuffle
 import jinja2
 import os
-#import requests
-import urllib
-import json 
-endpoint = 'https://maps.googleapis.com/maps/api/directions/json?'
-api_key = 'AIzaSyCXlRkL8rvN8cEiIV_t69tNAZdtKlbU6vY'
-#r = request.get(https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=AIzaSyCXlRkL8rvN8cEiIV_t69tNAZdtKlbU6vY, data = {'key':'value')
+# import urllib
+# import json 
+import requests
 
-#libraries for APIs
-from google.appengine.api import urlfetch
-import json
+search = raw_input('Type Name: ')
+
+url = "https://maps.googleapis.com/maps/api/geocode/json"
+
+params = {
+    'address': search
+}
+
+headers = {
+    'key': 'AIzaSyCXlRkL8rvN8cEiIV_t69tNAZdtKlbU6vY'
+}
+
+r = requests.get(url, headers=headers, params=params)
+
+print r.json()
+
+
+
+# # endpoint = 'https://maps.googleapis.com/maps/api/directions/json?'
+# # api_key = 'AIzaSyCXlRkL8rvN8cEiIV_t69tNAZdtKlbU6vY'
+
+# #libraries for APIs
+# from google.appengine.api import urlfetch
+# import json
 
 
 the_jinja_env = jinja2.Environment(
@@ -21,38 +39,38 @@ the_jinja_env = jinja2.Environment(
     
     
 class project(webapp2.RequestHandler):
-    def get(self):
-        results_template = the_jinja_env.get_template('/project.html')
-        #origin = input('What school are you in?')
-        #nav_request = 'origin=() & key=()'.format(origin,api_key)
-        #request = endpoint + nav_request
-        #response= urllib.request.urlopen(request).read()
-        #directions = json.loads(response)
-        self.response.write(results_template.render())
-    def post(self):
-        origin = self.request.get('origin')
-        self.response.write(origin)
-        nav_request = 'origin=() & key=()'.format(origin,api_key)
-        request = endpoint + nav_request
-        response = urllib.request.urlopen(request).read()
-        directions = json.loads(response)
-
+     def get(self):
+         results_template = the_jinja_env.get_template('/project.html')
+         #origin = input('What school are you in?')
+         #nav_request = 'origin=() & key=()'.format(origin,api_key)
+         #request = endpoint + nav_request
+         #response= urllib.request.urlopen(request).read()
+         #directions = json.loads(response)
+         self.response.write(results_template.render())
+     def post(self):
+         origin = self.request.get('school')
+         self.response.write(origin)
+#         nav_request = 'school=() & key=()'.format(origin,api_key)
+#         request = endpoint + nav_request
+#         response = urllib.urlopen(request).read()
+#         directions = json.loads(response)
+#         print(directions)
         
         
         
-college_select = {
-"UC Berkely": "", 
-"UC Davis": "",
-"UC Santa Cruz": "",
-"UC Irvine": "",
-"UC San Diego": "",
-"UC Riverside": "",
-"UCLA": "",
-"UC Merced": "",
-"UC Santa Barbara":"" 
-}
+# college_select = {
+# "UC Berkely": "", 
+# "UC Davis": "",
+# "UC Santa Cruz": "",
+# "UC Irvine": "",
+# "UC San Diego": "",
+# "UC Riverside": "",
+# "UCLA": "",
+# "UC Merced": "",
+# "UC Santa Barbara":"" 
+# }
 
         
 app = webapp2.WSGIApplication([
-    ('/', project),
-], debug=True)
+     ('/', project),
+ ], debug=True)
